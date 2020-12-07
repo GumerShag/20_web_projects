21acca1fbe66ef0b21642c282cc2693168846c75
Gumer Shagiyanov <gshagiyanov@aligntech.com>
2020-12-07 22:19:33 +0300

    feat: add fetching

--- Exchange Rate Calculator/script.js
+++ Exchange Rate Calculator/script.js
@@ -1,7 +1,12 @@
 const API_KEY = '28d1ac2983647afe6a5945b1';
 const BASE_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/`
+const firstSelect = document.getElementById('firstSelect');
+const firstAmount = document.getElementById('firstAmount');
+const secondSelect = document.getElementById('secondSelect');
+const secondAmount = document.getElementById('secondAmount');
+const rate = document.querySelector('.rate');
 const setAllCurrencyCodes = () => {
-    fetch(`${BASE_URL}/latest/USD`).then(async response => {
+    return fetch(`${BASE_URL}/latest/USD`).then(async response => {
         const data = await response.json();
         const firstSelect = document.getElementById('firstSelect');
         const secondSelect = document.getElementById('secondSelect');
@@ -13,20 +18,25 @@ const setAllCurrencyCodes = () => {
         secondSelect.innerHTML = optionList
     })
 }
+const getRates = (currency) => 
+    fetch(`${BASE_URL}/latest/${currency}`)
+    .then(res => res.json())
+    .then(data => data.conversion_rates)
 
-const calculate = () => {
-
+const calculate = async () => {
+    const currencyOne = firstSelect.value;
+    const currencyTwo = secondSelect.value;
+    const rates = await getRates(currencyOne);
+    rate.innerText = `1 ${currencyOne} = ${rates[currencyTwo]} ${currencyTwo}`;
+    secondAmount.value = (firstAmount.value * rates[currencyTwo]).toFixed(2);
 }
-const firstSelect = document.getElementById('firstSelect');
-const firstAmount = document.getElementById('firstAmount');
-const secondSelect = document.getElementById('secondSelect');
-const secondAmount = document.getElementById('secondAmount');
+
 
 firstSelect.addEventListener('change', calculate);
 firstAmount.addEventListener('input', calculate);
 secondSelect.addEventListener('change', calculate);
 secondAmount.addEventListener('input', calculate);
 onContentLoaded = () => {
-    setAllCurrencyCodes();
+    setAllCurrencyCodes().then(calculate);
 }
 document.addEventListener('DOMContentLoaded', onContentLoaded);
